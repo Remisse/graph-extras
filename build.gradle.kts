3e8fb9b4201ea65ebc9873f3dafacd442fb67117
Remisse <zeromuseg7@gmail.com>
2021-05-04 21:27:04 +0200

    Add HPPC and FastUtil

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,15 @@ dependencies {
     // Google Guava
     implementation("com.google.guava:guava:30.1.1-jre")
 
-    // Apache Commons
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("org.jetbrains:annotations:20.1.0")
 
+    // HPPC
+    implementation("com.carrotsearch:hppc:0.9.0.RC2")
+
+    // FastUtil
+    implementation("it.unimi.dsi:fastutil:8.5.4")
+
+    // JUnit Jupiter
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
